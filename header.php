e65bedc11d03709a8b157c6f9276084ee886af9c
unknown <jhonasemmanuel@gmail.com>
2022-07-07 15:48:18 +0800

    first commit

---
+++ header.php
@@ -0,0 +1,26 @@
+<header class="flex-row">
+    <img src="static/main/img/logoa.png" alt="" class="logo">
+    <nav id="header__nav-link" >
+        <ul class="nav__links flex-row">
+            <li class="links">New</li>
+            <li class="links">Men</li>
+            <li class="links">Women</li>
+        </ul>
+
+    </nav>
+    <nav id="header__nav-btn" >
+        <ul class="nav__btns flex-row">
+            <li class="btn">
+                <i class="fa-solid fa-user"></i>
+                <a href="#">Sign in</a>
+            </li>
+            <li class="btn">
+                <i class="fa-solid fa-cart-shopping"></i>
+                <a href="#">My Cart</a>
+            </li>
+        </ul>
+
+    </nav>
+
+
+</header>
